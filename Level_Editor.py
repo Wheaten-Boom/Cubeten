94b450b9bc2552e99f8c9b1cbd86f5250366cc79
manoritamar@gmail.com <manoritamar@gmail.com>
2022-08-02 19:01:19 +0300

    Added sprite selection with mouse (only adds outline rn)

--- Level_Editor.py
+++ Level_Editor.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 import os
 import json
+from select import select
 import pygame
 from pygame.locals import *
 import pygame_gui
@@ -16,10 +16,10 @@ def Data_Assembler(all_sprites):
     data = {"SUB_LEVELS": ["SUBLEVEL_0"], "SUBLEVEL_0": {"PLATFORMS": []}}
     for sprite in all_sprites:
         if type(sprite) == Platform:
-            new_platform = {"POS_X": int(sprite.rect.left) - 200,
+            new_platform = {"POS_X": int(sprite.rect.left) - 300,
                             "POS_Y": int(sprite.rect.top),
-                            "WIDTH": int(sprite.width),
-                            "HEIGHT": int(sprite.height),
+                            "WIDTH": int(sprite.rect.width),
+                            "HEIGHT": int(sprite.rect.height),
                             "COLOR": sprite.color,
                             "ID": sprite.ID,
                             "DRAW_LAYER": sprite.draw_layer}
@@ -29,17 +29,21 @@ def Data_Assembler(all_sprites):
     return data
 
 
-def update_sprite(mouse_x, mouse_y, original_x, original_y, sprite):
-    sprite.surf = pygame.Surface(
-        (abs(mouse_x - sprite.pos.x), abs(mouse_y - sprite.pos.y)))
-    sprite.surf.fill(sprite.color)
-    sprite.rect.topleft = (min(original_x, mouse_x), min(original_y, mouse_y))
-    sprite.width = abs(mouse_x - sprite.pos.x)
-    sprite.height = abs(mouse_y - sprite.pos.y)
+def is_mouse_on_sprite(sprite_list, mouse_pos):
+    for sprite in sprite_list:
+        if sprite.rect.collidepoint(mouse_pos):
+            return True
+    return False
+
+
+def draw_outline(sprite, displaysurface):
+    outline_rect = sprite.rect.copy()
+    outline_rect.inflate_ip((outline_rect.width + outline_rect.height) / 20,
+                            (outline_rect.width + outline_rect.height) / 20)
+    pygame.draw.rect(displaysurface, "0xFFFFFF", outline_rect, 0)
 
 
 def main():
-
     pygame.init()
     clock = pygame.time.Clock()
 
@@ -71,8 +75,11 @@ def main():
                                                object_id="#SAVE_BUTTON")
 
     all_sprites = pygame.sprite.Group()
-    recent_sprites = []
-    create_new_sprite = True
+    id_count = 0
+    current_sprite = None
+    original_draw_pos = None
+    creating_sprite = False
+    selected_sprite = None
 
     while True:
         pressed_keys = pygame.key.get_pressed()
@@ -88,37 +95,66 @@ def main():
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == clear_button:
                     all_sprites.empty()
-                    recent_sprites.clear()
-                    create_new_sprite = True
                 if event.ui_element == save_button:
                     data = Data_Assembler(all_sprites)
                     Create_Level_File(data, "TEST.json")
 
-            manager.process_events(event)
-
-        if pygame.mouse.get_pressed(num_buttons=3)[0] == True:
-            if pygame.mouse.get_pos()[0] > 300 and pygame.mouse.get_pos()[0] < 1300 and pygame.mouse.get_pos()[1] > 0 and pygame.mouse.get_pos()[1] < 1000:
-                if create_new_sprite == True:
-
-                    new_sprite = Platform(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1],
-                                          0, 0, "0x2c5160", 2, 1)
-                    all_sprites.add(new_sprite)
-                    recent_sprites.append(
-                        (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
-                    recent_sprites.append(new_sprite)
-                    create_new_sprite = False
-
+        # Check whether the mouse left click is pressed
+        if pygame.mouse.get_pressed()[0]:
+            mouse_pos = pygame.mouse.get_pos()
+            # Check if the mouse is on the drawing area
+            if mouse_pos[0] > 300 and mouse_pos[0] < 1300 and mouse_pos[1] > 0 and mouse_pos[1] < 1000:
+                # If we didn't already create a sprite, create one
+                if not creating_sprite:
+                    # If the mouse is on a sprite, select that sprite
+                    if is_mouse_on_sprite(all_sprites, mouse_pos):
+                        for sprite in all_sprites:
+                            if sprite.rect.collidepoint(mouse_pos):
+                                selected_sprite = sprite
+                                break
+                    else:
+                        creating_sprite = True
+                        selected_sprite = None
+                        current_sprite = Platform(
+                            mouse_pos[0], mouse_pos[1], 1, 1, "0x2c5160", id_count, 0)
+                        original_draw_pos = (mouse_pos[0], mouse_pos[1])
+                # If we did create one, update the sprite
                 else:
-                    update_sprite(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1],
-                                  recent_sprites[-2][0], recent_sprites[-2][1], recent_sprites[-1])
+                    current_sprite.rect.left = min(original_draw_pos[0],
+                                                   mouse_pos[0])
+                    current_sprite.rect.top = min(original_draw_pos[1],
+                                                  mouse_pos[1])
+                    current_sprite.rect.width = abs(mouse_pos[0]
+                                                    - original_draw_pos[0])
+                    current_sprite.rect.height = abs(mouse_pos[1]
+                                                     - original_draw_pos[1])
 
-        else:
-            create_new_sprite = True
+                    current_sprite.surf = pygame.Surface((current_sprite.rect.width,
+                                                          current_sprite.rect.height))
+                    current_sprite.surf.fill(current_sprite.color)
 
+        # If the mouse left click is not pressed, stop creating a sprite and add it to the sprite group
+        elif creating_sprite:
+            creating_sprite = False
+            all_sprites.add(current_sprite)
+            current_sprite = None
+            original_draw_pos = None
+            id_count += 1
+
+        manager.process_events(event)
         manager.update(clock.get_time())
 
         displaysurface.fill("0xAFDEEF")
 
+        # If we do have a sprite selected, draw an outline around it
+        if selected_sprite is not None:
+            draw_outline(selected_sprite, displaysurface)
+
+        # If we are creating a sprite, draw it
+        if current_sprite is not None:
+            displaysurface.blit(current_sprite.surf, current_sprite.rect)
+
+        # Draw all the sprites
         for entity in all_sprites:
             displaysurface.blit(entity.surf, entity.rect)
 
