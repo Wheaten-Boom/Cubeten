2c04ef34ded5577c16880695f841de20a4c386a3
manoritamar@gmail.com <manoritamar@gmail.com>
2022-09-15 18:10:36 +0300

    Switched quit command

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
+
 import level_runner
 import main_menu
 
 if __name__ == "__main__":
     level, screen = main_menu.main_menu()
     if level is None or screen is None:
-        quit()
+        sys.exit()
     level_runner.run_level(level, screen)
